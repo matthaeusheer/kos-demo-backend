85165805259b08284ef9a235431f24dc5a70889d
Zak Cook <zak.cook@ipt.ch>
2024-04-25 22:19:31 +0200

    remove hostname in additional info

--- src/kos_demo.py
+++ src/kos_demo.py
@@ -15,7 +15,8 @@ metrics.info("app_info", "Application info", version="1.0.3")
 @app.route("/win")
 def get_win_or_nowin() -> dict[str, str]:
     win_result = "win" if random.random() < PERCENTAGE_WIN else "nowin"
-    return {"win_result": win_result, "additional_info": os.environ["HOSTNAME"]}
+    additional_info = ""
+    return {"win_result": win_result, "additional_info": additional_info}
 
 
 @app.route("/liveness")
